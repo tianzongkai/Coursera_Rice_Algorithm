7a76ef626529aec2e28cfdaf1ff6636e8dc989b3
Zongkai <tianzongkai@gmail.com>
2018-03-12 23:07:23 -0400

    add

--- AlgorithmThinking_1/BFS.py
+++ AlgorithmThinking_1/BFS.py
@@ -1,3 +1,7 @@
+"""
+Code for BFS
+"""
+
 from collections import deque
 import random
 
@@ -10,23 +14,29 @@ def bfs_visited(ugraph, start_node):
     d.extendleft([0])
     d.extend([6,7,8]) -- extend from right
     """
-    Q = deque()
+    q = deque()
     visited = set([start_node])
-    Q.append(start_node)
-    while len(Q) > 0:
-        j = Q.popleft()
-        for neighbor in ugraph[j]:
+    q.append(start_node)
+    while len(q) > 0:
+        j_ = q.popleft()
+        for neighbor in ugraph[j_]:
             if neighbor not in visited:
                 visited.add(neighbor)
-                Q.append(neighbor)
+                q.append(neighbor)
     return visited
 
 def cc_visited(ugraph):
-    RemainingNodes = ugraph.keys()
-    CC = set()
-    while len(RemainingNodes) > 0:
-        i = random.choice(RemainingNodes)
-        W = bfs_visited(ugraph, i)
-        CC.union(W)
-        RemainingNodes = list(set(RemainingNodes) - W)
-    return CC
+    """
+    Implementation for cc_visited algorithm
+    :param ugraph: undirected graph
+    :return: a list of sets, each set consists of all the nodes (and nothing else)
+    in a connected component
+    """
+    remaining_nodes = ugraph.keys()
+    c_c = []
+    while len(remaining_nodes) > 0:
+        i_ = random.choice(remaining_nodes)
+        w_ = bfs_visited(ugraph, i_)
+        c_c.append(w_)
+        remaining_nodes = list(set(remaining_nodes) - w_)
+    return c_c
