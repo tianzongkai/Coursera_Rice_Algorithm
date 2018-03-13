c6a39e7677471d8913aca96ab67e658d2a719cd8
Zongkai <tianzongkai@gmail.com>
2018-03-13 00:10:37 -0400

    add

--- AlgorithmThinking_1/BFS.py
+++ AlgorithmThinking_1/BFS.py
@@ -51,9 +51,46 @@ def largest_cc_size(ugraph):
     :return:  the size (an integer) of the largest connected component in ugraph
     """
     connected_components = cc_visited(ugraph)
-    largest_size = float('-inf')
+    largest_size = 0
     for component in connected_components:
         size = len(component)
         if size > largest_size:
             largest_size = size
     return largest_size
+
+def compute_resilience(ugraph, attack_order):
+    """
+    remove these nodes (and their edges) from the graph one at a time
+    and then measure the "resilience" of the graph at each removal by
+    computing the size of its largest remaining connected component.
+    :param ugraph: undirected graph
+    :param attack_order: a list of nodes
+    :return: a list whose k+1th entry is the size of the largest connected component
+    in the graph after the removal of the first k nodes in attack_order.
+    The first entry (indexed by zero) is the size of the largest connected component
+     in the original graph
+    """
+
+    sizes_after_removal = [largest_cc_size(ugraph)]
+    for attack_node in attack_order:
+        # print attack_node
+        if attack_node in ugraph:
+            del ugraph[attack_node]
+            # print ugraph
+            for node, edges in ugraph.iteritems():
+                if attack_node in edges:
+                    edges.remove(attack_node)
+                    ugraph[node] = edges
+            # print ugraph
+        sizes_after_removal.append(largest_cc_size(ugraph))
+    return sizes_after_removal
+
+# ugraph = {0: set([1,2]),
+#           1: set([3]),
+#           2: set([2]),
+#           3: set([1,2,3]),
+#           4: set([])
+#           }
+# attack_order = [1,2]
+# print largest_cc_size(ugraph)
+# print compute_resilience(ugraph, attack_order)
