07236f6a8fcd9cfa9e9242344f5bd79ef71eaf95
Zongkai <tianzongkai@gmail.com>
2018-05-15 15:00:08 -0400

    assignment2

--- AlgorithmThinking_2/myCode.py
+++ AlgorithmThinking_2/myCode.py
@@ -60,8 +60,28 @@ def fast_closest_pair(cluster_list):
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.
     """
+    n = len(cluster_list)
+    if n <= 3:
+        ret_d = slow_closest_pair(cluster_list)
+    else:
+        ### divide ###
+        m = n / 2
+        cluster_l = cluster_list[0:m]
+        cluster_r = cluster_list[m:n]
+        ret_l = fast_closest_pair(cluster_l)
+        ret_r = fast_closest_pair(cluster_r)
 
-    return ()
+        ### merge ###
+        if ret_l[0] <= ret_r[0]:
+            ret_d = ret_l
+        else:
+            ret_d = (ret_r[0], ret_r[1] + m, ret_r + m)
+        mid = (cluster_list[m-1].horiz_center() + cluster_list[m].horiz_center()) / 2
+        ret_cps = closest_pair_strip(cluster_list, mid, ret_d[0])
+        if ret_cps[0] < ret_d[0]:
+            ret_d = ret_cps
+
+    return ret_d
 
 
 def closest_pair_strip(cluster_list, horiz_center, half_width):
@@ -76,8 +96,18 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] lie in the strip and have minimum distance dist.
     """
-
-    return ()
+    # [ num for num in numbers if num < 5 ]
+    s = filter(lambda idx: cluster_list[idx].horiz_center() < half_width,
+               range(len(cluster_list)))
+    s.sort(key=lambda cluster: cluster.vert_center())
+    k = len(s)
+    ret_d = (float('inf'), -1, -1)
+    for u in range(k-3):
+        for v in range(u+1, min(u+3,k-1)):
+            ret_s = pair_distance(cluster_list, s[u], s[v])
+            if ret_s[0] < ret_d[0]:
+                ret_d = ret_s
+    return ret_d
 
 
 
