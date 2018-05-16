9df0ff4e38d05301259672a60c001c09fae8f75c
Zongkai <tianzongkai@gmail.com>
2018-05-16 17:59:51 -0400

    assignment2

--- AlgorithmThinking_2/application3.py
+++ AlgorithmThinking_2/application3.py
@@ -43,3 +43,66 @@ def compute_distortion(cluster_list, data_table):
     distortion_list = [cluster.cluster_error(data_table) for cluster in cluster_list]
     distortion = sum(distortion_list)
     return distortion
+
+# Question 10
+DIRECTORY = "http://commondatastorage.googleapis.com/codeskulptor-assets/"
+DATA_3108_URL = DIRECTORY + "data_clustering/unifiedCancerData_3108.csv"
+DATA_896_URL = DIRECTORY + "data_clustering/unifiedCancerData_896.csv"
+DATA_290_URL = DIRECTORY + "data_clustering/unifiedCancerData_290.csv"
+DATA_111_URL = DIRECTORY + "data_clustering/unifiedCancerData_111.csv"
+DATA_24_URL = DIRECTORY + "data_clustering/unifiedCancerData_24.csv"
+
+
+def load_data_table(data_url):
+    """
+    Import a table of county-based cancer risk data
+    from a csv format file
+    """
+    import urllib2
+    data_file = urllib2.urlopen(data_url)
+    data = data_file.read()
+    data_lines = data.split('\n')
+    print "Loaded", len(data_lines), "data points"
+    data_tokens = [line.split(',') for line in data_lines]
+    return [[tokens[0], float(tokens[1]), float(tokens[2]), int(tokens[3]), float(tokens[4])]
+            for tokens in data_tokens]
+
+def clustering():
+    title_list = ['111 counties', '290 counties', '896 counties']
+    url_list = [DATA_111_URL,DATA_290_URL,DATA_896_URL]
+    distortion_hierarchical = [[],[],[]]
+    distortion_kmeans = [[],[],[]]
+    num_clusters_list = range(20, 5, -1)
+
+    for idx in range(len(url_list)):
+        data_table = load_data_table(url_list[idx])
+        cluster_list = []
+        for line in data_table:
+            cluster_list.append(alg_cluster.Cluster(set([line[0]]), line[1], line[2], line[3], line[4]))
+        cluster_list_copy = [cluster.copy() for cluster in cluster_list]
+        for num_cluster in num_clusters_list:
+            cluster_list = student.hierarchical_clustering(cluster_list, num_cluster)
+            distortion = compute_distortion(cluster_list, data_table)
+            distortion_hierarchical[idx].append(distortion)
+            print "Displaying", len(cluster_list), "hierarchical clusters, distortion:", distortion
+
+        for num_cluster in num_clusters_list:
+            cluster_list = student.kmeans_clustering(cluster_list_copy, num_cluster, 5)
+            distortion = compute_distortion(cluster_list, data_table)
+            distortion_kmeans[idx].append(distortion)
+            print "Displaying", len(cluster_list), "k-means clusters, distortion:", distortion
+
+        plot_num = 131 + idx
+        plt.subplot(plot_num)
+        plt.plot(num_clusters_list, distortion_hierarchical[idx], "o-", label="hierarchical")
+        plt.plot(num_clusters_list, distortion_kmeans[idx], "x-", label="kmeans")
+        plt.legend()
+        plt.ylabel('Distortion')
+        plt.xlabel('Number of clusters')
+        plt.grid(True)
+        plt.title(title_list[idx])
+    plt.show()
+
+clustering()
+
+
