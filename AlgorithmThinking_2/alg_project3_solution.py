8f9856f9374b29b5358e44660936e1123ef7fb7f
Zongkai <tianzongkai@gmail.com>
2018-05-16 00:33:25 -0400

    assignment2

--- AlgorithmThinking_2/alg_project3_solution.py
+++ AlgorithmThinking_2/alg_project3_solution.py
@@ -40,15 +40,14 @@ def slow_closest_pair(cluster_list):
     cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.
     """
     ret_d = (float('inf'), -1, -1)
-    for u in range(len(cluster_list)):
-        for v in range(len(cluster_list)):
-            if u != v:
-                current_d = pair_distance(cluster_list, u, v)
+    for u_idx in range(len(cluster_list)):
+        for v_idx in range(len(cluster_list)):
+            if u_idx != v_idx:
+                current_d = pair_distance(cluster_list, u_idx, v_idx)
                 if current_d[0] < ret_d[0]:
                     ret_d = current_d
 
     return ret_d
-
 
 def fast_closest_pair(cluster_list):
     """
@@ -60,14 +59,14 @@ def fast_closest_pair(cluster_list):
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.
     """
-    n = len(cluster_list)
-    if n <= 3:
+    n_len = len(cluster_list)
+    if n_len <= 3:
         ret_d = slow_closest_pair(cluster_list)
     else:
         ### divide ###
-        m = n / 2
-        cluster_l = cluster_list[0:m]
-        cluster_r = cluster_list[m:n]
+        mid = n_len / 2
+        cluster_l = cluster_list[0:mid]
+        cluster_r = cluster_list[mid:n_len]
         ret_l = fast_closest_pair(cluster_l)
         ret_r = fast_closest_pair(cluster_r)
 
@@ -75,14 +74,13 @@ def fast_closest_pair(cluster_list):
         if ret_l[0] <= ret_r[0]:
             ret_d = ret_l
         else:
-            ret_d = (ret_r[0], ret_r[1] + m, ret_r + m)
-        mid = (cluster_list[m-1].horiz_center() + cluster_list[m].horiz_center()) / 2
+            ret_d = (ret_r[0], ret_r[1] + mid, ret_r[2] + mid)
+        mid = (cluster_list[mid-1].horiz_center() + cluster_list[mid].horiz_center()) / 2
         ret_cps = closest_pair_strip(cluster_list, mid, ret_d[0])
         if ret_cps[0] < ret_d[0]:
             ret_d = ret_cps
-
+    # ret_d :(dist, idx1, idx2)
     return ret_d
-
 
 def closest_pair_strip(cluster_list, horiz_center, half_width):
     """
@@ -96,35 +94,20 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] lie in the strip and have minimum distance dist.
     """
-    # [ num for num in numbers if num < 5 ]
-    s = filter(lambda idx: cluster_list[idx].horiz_center() < half_width,
+    s_cluster = filter(lambda idx: abs(cluster_list[idx].horiz_center() - horiz_center) < half_width,
                range(len(cluster_list)))
-    s.sort(key=lambda cluster: cluster.vert_center())
-    k = len(s)
+    # print 'original s', s
+    s_cluster.sort(key=lambda idx: cluster_list[idx].vert_center())
+    # print 'sorted s', s
+    k_len = len(s_cluster)
+    # print 'k =', k
     ret_d = (float('inf'), -1, -1)
-    for u in range(k-3):
-        for v in range(u+1, min(u+3,k-1)):
-            ret_s = pair_distance(cluster_list, s[u], s[v])
+    for u_idx in range(k_len-1):
+        for v_idx in range(u_idx+1, min(u_idx+3,k_len-1)+1):
+            ret_s = pair_distance(cluster_list, s_cluster[u_idx], s_cluster[v_idx])
             if ret_s[0] < ret_d[0]:
                 ret_d = ret_s
     return ret_d
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ######################################################################
 # Code for hierarchical clustering
@@ -138,8 +121,15 @@ def hierarchical_clustering(cluster_list, num_clusters):
     Input: List of clusters, integer number of clusters
     Output: List of clusters whose length is num_clusters
     """
+    cluster_list.sort(key=lambda cluster: cluster.horiz_center())
+    # n = len(cluster_list)
+    while (len(cluster_list) > num_clusters):
+        _dest, i_idx, j_idx = fast_closest_pair(cluster_list)
+        cluster_list[i_idx] = cluster_list[i_idx].merge_clusters(cluster_list[j_idx])
+        cluster_list.remove(cluster_list[j_idx])
+        cluster_list.sort(key=lambda cluster: cluster.horiz_center())
 
-    return []
+    return cluster_list
 
 
 ######################################################################
@@ -157,5 +147,18 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
 
     # position initial clusters at the location of clusters with largest populations
 
-    return []
+    n_len = len(cluster_list)
+    list_copy = list(cluster_list)
+    list_copy.sort(key=lambda cluster: cluster.total_population(), reverse=True)
+    old_cluster = [cluster.copy() for cluster in list_copy[0:num_clusters]]
+    # cluster_list.sort(key=lambda cluster: cluster.horiz_center())
+    for _ in range(num_iterations):
+        new_cluster = [alg_cluster.Cluster(set([]),0,0,0,0) for _ in range(num_clusters)]
+        for j_idx in range(n_len):
+            dist_list = [(cluster_list[j_idx].distance(old_cluster[f]), f) for f in range(num_clusters)]
+            dist_list.sort(key=lambda dist_tuple: dist_tuple[0])
+            argmin_closest = dist_list[0][1]
+            new_cluster[argmin_closest].merge_clusters(cluster_list[j_idx])
+        old_cluster = [c_i.copy() for c_i in new_cluster]
+    return new_cluster
 
