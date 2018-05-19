8a6208138a179443469fbefdd263b8c81bff990d
Zongkai <tianzongkai@gmail.com>
2018-05-19 16:31:31 -0400

    project4

--- AlgorithmThinking_2/DynamicProgramming/application4_solution.py
+++ AlgorithmThinking_2/DynamicProgramming/application4_solution.py
@@ -94,18 +94,67 @@ def find_local_align():
     for idx in range(length):
         if seq_loc_fly[idx] == seq_loc_human[idx]:
             agree += 1
-
+    print 'Question 1:\n'
     print 'score:', score, '\nhuman:', seq_loc_human, '\nfly:  ', seq_loc_fly
     print 'Agree percentage: %.2f' % (100*float(agree)/length)
+    """
+    Question 1:
+    score: 875 
+    human: HSGVNQLGGVFVNGRPLPDSTRQKIVELAHSGARPCDISRILQVSNGCVSKILGRYYETGSIRPRAIGGSKPRVATPEVVSKIAQYKRECPSIFAWEIRDRLLSEGVCTNDNIPSVSSINRVLRNLASEK-QQ 
+    fly:   HSGVNQLGGVFVGGRPLPDSTRQKIVELAHSGARPCDISRILQVSNGCVSKILGRYYETGSIRPRAIGGSKPRVATAEVVSKISQYKRECPSIFAWEIRDRLLQENVCTNDNIPSVSSINRVLRNLAAQKEQQ
+    Agree percentage: 93.98%
+    """
+
+    ### Question 2 ###
+    print '\nQuestion 2:\n'
+    seq_loc_human = seq_loc_human.replace('-','')
+    seq_loc_fly = seq_loc_fly.replace('-','')
+    seq_pax = read_protein(CONSENSUS_PAX_URL)  #Q2
+    # seq_pax = 'ACBEDGFIHKMLNQPSRTWVYXZ' #Q3
+    for idx in range(2):
+        if idx == 0:
+            seq = seq_loc_human
+            type = 'human'
+        else:
+            seq = seq_loc_fly
+            type = 'fly'
+        global_alignment_matrix = student.compute_alignment_matrix(seq,seq_pax,score_matrix,True)
+        score, x_glbl, pax_glbl = student.compute_global_alignment(seq,seq_pax,score_matrix,global_alignment_matrix)
+        length = len(x_glbl)
+        agree = 0
+        for idx in range(length):
+            if x_glbl[idx] == pax_glbl[idx]:
+                agree += 1
+
+        print 'score:', score, '\n'+type, x_glbl, '\nPAX:  ', pax_glbl
+        print type+' agree percentage: %.2f' % (100 * float(agree) / length)
+        """
+        Question 2:
+
+        human score: 613 
+        human: -HSGVNQLGGVFVNGRPLPDSTRQKIVELAHSGARPCDISRILQVSNGCVSKILGRYYETGSIRPRAIGGSKPRVATPEVVSKIAQYKRECPSIFAWEIRDRLLSEGVCTNDNIPSVSSINRVLRNLASEKQQ 
+        PAX:   GHGGVNQLGGVFVNGRPLPDVVRQRIVELAHQGVRPCDISRQLRVSHGCVSKILGRYYETGSIKPGVIGGSKPKVATPKVVEKIAEYKRQNPTMFAWEIRDRLLAERVCDNDTVPSVSSINRIIR--------
+        human agree percentage: 72.93
+        
+        flyscore: 586 
+        fly:  -HSGVNQLGGVFVGGRPLPDSTRQKIVELAHSGARPCDISRILQVSNGCVSKILGRYYETGSIRPRAIGGSKPRVATAEVVSKISQYKRECPSIFAWEIRDRLLQENVCTNDNIPSVSSINRVLRNLAAQKEQQ 
+        PAX:  GHGGVNQLGGVFVNGRPLPDVVRQRIVELAHQGVRPCDISRQLRVSHGCVSKILGRYYETGSIKPGVIGGSKPKVATPKVVEKIAEYKRQNPTMFAWEIRDRLLAERVCDNDTVPSVSSINRIIR---------
+        fly agree percentage: 70.15
+        """
+
+        """
+        Question 3:
+        
+        The level of similarity of Q1 and Q2 is not due to chance, but due to mutation
+         from the same ancestor over long time.
+        
+        For two random sequences of similar length, the agreement of alighment wouldn't be
+        as high as Q1 and Q2 becasue random sequences have very low chance that they're 
+        mutated from the same sequence. 
+        """
 
 
 find_local_align()
-"""
-score: 875 
-human: HSGVNQLGGVFVNGRPLPDSTRQKIVELAHSGARPCDISRILQVSNGCVSKILGRYYETGSIRPRAIGGSKPRVATPEVVSKIAQYKRECPSIFAWEIRDRLLSEGVCTNDNIPSVSSINRVLRNLASEK-QQ 
-fly:   HSGVNQLGGVFVGGRPLPDSTRQKIVELAHSGARPCDISRILQVSNGCVSKILGRYYETGSIRPRAIGGSKPRVATAEVVSKISQYKRECPSIFAWEIRDRLLQENVCTNDNIPSVSSINRVLRNLAAQKEQQ
-Agree percentage: 93.98%
-"""
 
 
-### Question 2 ###
+
